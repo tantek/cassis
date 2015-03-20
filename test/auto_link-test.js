bfd0da7aeec248728b88a3a0e5aea96e97fe8da0
Bret Comnes <bcomnes@gmail.com>
2015-03-20 16:12:05 -0700

    Added semicolins.  Woops forgot those.

--- test/auto_link-test.js
+++ test/auto_link-test.js
@@ -1,20 +1,20 @@
-var test = require('tape')
+var test = require('tape');
 
-var auto_link = require('../cassis').auto_link
+var auto_link = require('../cassis').auto_link;
 
 var noops = [
   { 
     str: 'This string definately does not have any urls in it',
     msg: 'noop on strings without links'
   }
-]
+];
 
 test('auto_link noops', function (t) {
   t.plan(noops.length)
   noops.forEach(function (obj) {
     t.equal(auto_link(obj.str), obj.str, obj.msg)
-  })
-})
+  });
+});
 
 var links = [
   {
@@ -35,11 +35,11 @@ var links = [
     '<a class="auto-link" href="http://nerd.io">nerd.io</a> link',
     msg: 'auto_link simple .io links'
   }
-]
+];
 
 test('singleline transforms', function (t) {
   t.plan(links.length)
   links.forEach(function (obj) {
     t.equal(auto_link(obj.str), obj.expect, obj.msg)
-  })
-})
+  });
+});
